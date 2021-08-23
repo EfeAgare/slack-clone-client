dd4daa98db96623c59527aaa8a9ec39cfd5404a6
Efe Agare <knowledgeagare157@gmail.com>
2021-08-23 20:49:15 +0100

    add comment

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 cd /home/ec2-user/server
+# install dependencies
 npm install
+
+# install create-react-app and react-scripts
+# without react-scripts application cannot be started
 npm install --save react react-dom react-scripts react-particles-js
+
+# install pm2 process manage
 npm install pm2 -g
--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,6 +1,12 @@
 cd /home/ec2-user/server
 sudo npm run-script build
 pm2 start npm --name "slack-clone" -- start
+
+# auto restart server if shut down
 pm2 startup
+
+# freeze process list for automatic respawn
 pm2 save
+
+# restart all processes - necessary to do this again?
 pm2 restart all
